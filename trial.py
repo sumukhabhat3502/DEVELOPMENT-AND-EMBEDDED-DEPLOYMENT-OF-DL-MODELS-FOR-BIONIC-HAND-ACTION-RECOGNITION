0c2044577fcfc377a1908ae0281d9f4a562f20a6
Sumukha U <sumukhabhat02@gmail.com>
2023-08-03 18:42:07 +0530

    trail values printing static plot

--- trial.py
+++ trial.py
@@ -2,12 +2,11 @@ import tkinter as tk
 from tkinter import messagebox, filedialog
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import serial
 import threading
-from matplotlib import animation
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import *
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from serial.tools import list_ports
 
 root = tk.Tk()
@@ -27,7 +26,6 @@ plt.rcParams.update({
     "ytick.labelsize": ticklabelsize,
     "figure.figsize": [12.50, 8.20],
     "figure.facecolor": "lightgray"})
-
 
 click_action = tk.StringVar()
 click_action.set("Cylindrical")
@@ -55,37 +53,33 @@ frm.place(x=370, y=400, anchor='w')
 
 # Create three subplots for each sensor
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 10))
-# Initialize empty lists for data
-data1, data2, data3 = [], [], []
+data1, data2, data3 = [], [], []  # Initialize empty lists for data
 
 # Create line objects for each plot
-line1, = ax1.plot([], [], label="Sensor 1")
-line2, = ax2.plot([], [], label="Sensor 2")
-line3, = ax3.plot([], [], label="Sensor 3")
+line1, = ax1.plot([], [], 'r-', label='Sensor 1')
+line2, = ax2.plot([], [], 'g-', label='Sensor 2')
+line3, = ax3.plot([], [], 'b-', label='Sensor 3')
+
+# Set axis limits
+ax1.set_xlim(0, 1000)
+ax2.set_xlim(0, 1000)
+ax3.set_xlim(0, 1000)
+
+# Set labels and legends
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Sensor 1 Value')
+ax2.set_xlabel('Time')
+ax2.set_ylabel('Sensor 2 Value')
+ax3.set_xlabel('Time')
+ax3.set_ylabel('Sensor 3 Value')
+ax1.legend()
+ax2.legend()
+ax3.legend()
 
 canvas = FigureCanvasTkAgg(fig, master=frm)
 canvas.draw()
 canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-ax1.set_xlim([0, 100])
-ax1.set_ylim(-500, 500)
-ax1.plot([], [], "lime")
-ax1.grid()
-ax1.set_title('Channel 1')
-
-ax2.set_xlim([0, 100])
-ax2.set_ylim(-500, 500)
-ax2.plot([], [], "lime")
-ax2.grid()
-ax2.set_title('Channel 2')
-
-ax3.set_xlim([0, 100])
-ax3.set_ylim(-500, 500)
-ax3.plot([], [], "lime")
-ax3.grid()
-ax3.set_title('Channel 3')
-sensor_names = ["Sensor 1", "Sensor 2", "Sensor 3"]
-values_plotted=0
 # Serial communication thread
 serial_thread = None  # Initialize serial thread variable
 class SerialThread(threading.Thread):
@@ -99,23 +93,24 @@ class SerialThread(threading.Thread):
     def run(self):
         entry_count = 0  # Number of entries recorded
         try:
-            while self.running and entry_count != 1100:
+            while self.running and entry_count != 1001:
                 if not self.paused:
                     try:
                         line = self.serial_port.readline().decode().strip()
-                        # print(line)
                         if line:
                             values = line.split(',')
                             if len(values) == 3:
                                 sensor1 = int(values[0])
                                 sensor2 = int(values[1])
                                 sensor3 = int(values[2])
-                                print("Sensor 1:", sensor1, "Sensor 2:", sensor2, "Sensor 3:", sensor3)
+                                print("sensor1: ",sensor1)
+                                print("sensor2: ",sensor2)
+                                print("sensor3: ",sensor3)
+                                data1.append(sensor1)
+                                data2.append(sensor2)
+                                data3.append(sensor3)
 
-                                # self.data_queue.append((sensor1, sensor2, sensor3))
                                 entry_count += 1
-                        # print("sensor_queue")
-                        # print(self.data_queue)
                     except UnicodeDecodeError:
                         messagebox.showerror("UnicodeDecodeError", "An error occurred while decoding the serial data.")
                         self.resume()
@@ -124,9 +119,13 @@ class SerialThread(threading.Thread):
         except Exception as e:
             messagebox.showerror("Error", str(e))
         finally:
-            # print(entry_count)
-            if entry_count == 1100:
+            if entry_count == 1001:
+                print("data1:",data1)
+                print("data2:",data2)
+                print("data3:",data3)
                 Accept_button.configure(state='normal')
+                # Call the function to plot the data once data collection is complete
+                plot_data()
             self.serial_port.close()  # Close the serial port
 
     def stop(self):
@@ -142,16 +141,12 @@ def get_available_ports():
     ports = list_ports.comports()
     return [port.device for port in ports]
 
-
-
 def start_serial_communication():
     global serial_thread
 
     # Get the selected action from the drop-down menu
     selected_action = click_action.get()
     print("Selected Action:", selected_action)
-
-    # TODO: You might want to configure the serial_port and other settings here
 
     try:
         # Create and start the serial communication thread
@@ -165,13 +160,19 @@ def start_serial_communication():
 # Associate the start_serial_communication function with the "Start" button click event
 Start_button.configure(command=start_serial_communication)
 
-# ... (existing code below)
+def plot_data():
+    # Plot the data once data collection is complete
+    ax1.plot(data1, 'r-', label='Sensor 1')
 
+    ax2.plot(data2, 'g-', label='Sensor 2')
+    ax3.plot(data3, 'b-', label='Sensor 3')
 
+    # Set axis limits again
+    ax1.set_xlim(0, len(data1))
+    ax2.set_xlim(0, len(data2))
+    ax3.set_xlim(0, len(data3))
+
+    # Update the plot
+    canvas.draw()
 
 root.mainloop()
-
-
-
-
-
